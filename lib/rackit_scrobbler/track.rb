ce3dfe67b95294ce9492e3bdcc00b6d98e2f5a85
Bob Van Landuyt <bob.vanlanduyt@gmail.com>
2013-03-08 09:42:44 +0100

    Dont scrouble multiple times

--- lib/rackit_scrobbler/track.rb
+++ lib/rackit_scrobbler/track.rb
@@ -1,11 +1,15 @@
 require "active_support/all"
 module RackitScrobbler
   class Track
-    attr_accessor :artist, :title, :album, :route, :partner_id, :duration
+    attr_accessor :artist, :title, :album, :route, :partner_id, :duration, :scrobbled
     def initialize(params = {})
       params.keys.each do |k|
         send("#{k.underscore}=", params[k])
       end
+    end
+
+    def to_s
+      "#{artist}: #{album} - #{title}"
     end
 
   end
