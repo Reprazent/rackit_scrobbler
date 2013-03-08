32ac4c2e1c878526fe804061a52b66beb5abc291
Bob Van Landuyt <bob.vanlanduyt@gmail.com>
2013-03-08 09:42:10 +0100

    Remove progressbar

--- lib/rackit_scrobbler/player.rb
+++ lib/rackit_scrobbler/player.rb
@@ -1,6 +1,6 @@
 module RackitScrobbler
   class Player
-    attr_accessor :current_track, :last_fm, :track_started_time, :progressbar
+    attr_accessor :current_track, :last_fm, :track_started_time
 
     def initialize(last_fm_client)
       @last_fm = last_fm_client
@@ -12,22 +12,21 @@ module RackitScrobbler
     end
     alias_method :resume, :playing
 
-    def paused
+    def pause
       puts "Paused #{current_track}"
     end
 
     def start
-      progressbar = ProgressBar.create
       track_started_time = Time.now
       playing
     end
 
     def ping(percentage)
-      if percentage > 0.30
+      if current_track && (percentage > 0.30) && !current_track.scrobbled
         last_fm.scrobble(current_track, track_started_time)
+        current_track.scrobbled = true
         puts "Scrobbled #{current_track}"
       end
-      progressbar.progress = percentage * 100
     end
 
   end
